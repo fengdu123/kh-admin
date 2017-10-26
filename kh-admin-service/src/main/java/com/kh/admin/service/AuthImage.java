39a66d8050cb282e3a91f03f12849b10f8c1742e
wangyp <13296538826@163.com>
2017-10-26 20:41:03 +0800

    java后台实现验证码

---
+++ kh-admin-service/src/main/java/com/kh/admin/service/AuthImage.java
@@ -0,0 +1,51 @@
+package com.kh.admin.service;
+
+import com.kh.admin.common.utils.VerifyCodeUtils;
+
+import javax.servlet.Servlet;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+/**
+ * 所在的包名: com.kh.admin.service
+ * 所在的项目名：kh-admin
+ *
+ * @Author:wangyp
+ * @Description:  java生成二维码
+ * @Date: Created in 17:27 2017/10/26
+ */
+public class AuthImage extends HttpServlet implements Servlet{
+
+    static final long serialVersionUID = 1L;
+
+    /**
+     * 生成验证码
+     * @param request
+     * @param response
+     * @throws ServletException
+     * @throws IOException
+     */
+    @Override
+    public void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setHeader("Pragma", "No-cache");
+        response.setHeader("Cache-Control", "no-cache");
+        response.setDateHeader("Expires", 0);
+        response.setContentType("image/jpeg");
+
+        //生成随机字串
+        String verifyCode = VerifyCodeUtils.generateVerifyCode(4);
+        //存入会话sessions
+        HttpSession session = request.getSession(true);
+        //删除以前的
+        session.removeAttribute("verCode");
+        session.setAttribute("verCode", verifyCode.toLowerCase());
+        //生成图片
+        int w = 100, h = 30;
+        VerifyCodeUtils.outputImage(w, h, response.getOutputStream(), verifyCode);
+
+    }
+}
